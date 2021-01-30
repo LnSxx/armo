31f0b9630b906710e93a234e2e1b5bd621ce7f4d
Leonid Semenov <ln.sm.study@gmail.com>
2021-01-30 18:14:39 +0300

    Add installation of python dependencies & make sh scripts executable.

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-sudo pacman -S npm python
+sudo pacman -S --needed npm python electron
+cd backend
+python -m venv .venv
+source .venv/bin/activate 
+pip install -r requirements.txt
+cd ..
 cd frontend
 npm install
+cd ..
